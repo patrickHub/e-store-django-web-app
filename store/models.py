7b013054218846ffdee0fe0e15f451bc70fd694e
patrickHub <patrick.djomo@heig-vd.ch>
2019-07-09 13:47:23 +0200

    add product + category model

--- store/models.py
+++ store/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class Category(models.Model):
+
+    categoryID = models.AutoField(primary_key=True)
+    categoryName = models.CharField(max_length=30)
+
+
+class Product(models.Model):
+
+    productID = models.AutoField(primary_key=True)
+    productName = models.CharField(max_length=30)
+    productPrice = models.FloatField()
+    productDescription = models.TextField()
+    productImgPath = models.CharField(max_length=100)
+    categoryID = models.ForeignKey(
+        Category, related_name='+', on_delete=models.CASCADE)
